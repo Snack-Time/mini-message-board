908d0e170f3ef02d5dda9ead5d21fedfe6ed0cc8
Snack-Time <wmcnally1226@gmail.com>
2024-02-28 15:58:01 -0500

    moved code from new route to index route

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: "Mini Messageboard", messages: messages })
 });
 
+router.get("/new", function(req, res, next) {
+  res.render('form', {title: "New Message"})
+});
+
+router.post("/new", function(req, res, next) {
+  messages.push({
+      text: req.body.text,
+      user: req.body.name,
+      date: new Date(),
+  })
+  res.redirect('/')
+})
+
+
 module.exports = router;
