73ff26254d906fb2f875307b8d50dd86187d730c
Snack-Time <wmcnally1226@gmail.com>
2024-02-28 15:24:03 -0500

    add test messages and input form route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "You smell.",
+    user: "Dwayne",
+    date: new Date(),
+  },
+  {
+    text: "Acknowledge me.",
+    user: "Roman",
+    date: new Date(),
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Mini Messageboard", messages: messages })
 });
 
 module.exports = router;
